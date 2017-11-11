b659ae647b9dc8ab7df87d4c0a666ffe56c16196
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2017-11-11 17:03:31 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const prettierConfig = require('./prettier.config');
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true
+  },
+  rules: {
+    'prettier/prettier': ['warn', prettierConfig],
+    'react/prop-types': ['warn', {skipUndeclared: true}],
+    'react/jsx-no-target-blank': 'warn',
+    'react/no-unescaped-entities': false,
+    'react/display-name': false
+  },
+  globals: {
+    shallow: false,
+    render: false,
+    mount: false
+  }
+};
